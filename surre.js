7b0552f514355a104906241318adadb915e19586
RaulSAlves7 <raulsalves63@gmail.com>
2024-06-13 15:46:01 -0300

    Add files via upload

---
+++ surre.js
@@ -0,0 +1,10 @@
+function Myfunction(){
+    window.location.href = "https://emoff.correiobraziliense.com.br/famosos/amigo-de-neymar-luciano-huck-ja-foi-condenado-por-privatizar-praia/#:~:text=Em%20outras%20palavras%2C%20a%20juíza,o%20processo%20em%20segunda%20instância.";
+  };
+  function redirect(){
+    window.location.href = "surre1.html"
+  }
+  function clique(){
+    (prompt("Digite seu feedback"))
+    alert("Seu feedback foi enviado com sucesso")
+  }
